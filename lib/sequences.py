8f82e90f3bb42c751bb159c222cb854c38fb8a47
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-07-28 12:19:20 -0400

    lab Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibo =[0,1];
+    a=[]
+    b=[0]
+    num1=0;
+    num2=1;
+    i=2;
+
+    while(i<length):
+        sum=num1+num2;
+        fibo.append(sum);
+        num1=num2;
+        num2=sum;
+        i=i+1;
+    
+    if length == 0:
+        print(a);
+    elif length == 1:
+        print(b);
+    else:
+        print(fibo);
